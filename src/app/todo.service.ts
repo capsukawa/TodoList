dfaf59bf63c4eeac7dc7d2006492eb6c5d397d9b
Unknown <arnaud-av@live.fr>
2019-03-27 17:18:05 +0100

    Ordre listes + erreur si meme nom

--- src/app/todo.service.ts
+++ src/app/todo.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import 'rxjs/Rx';
 import uuid from 'uuid/v4';
+import { AlertController } from '@ionic/angular';
 
 
 export interface TodoList {
@@ -71,7 +72,7 @@ export class TodoServiceProvider {
     }
   ];
 
-  constructor(private db: AngularFirestore) {
+  constructor(private db: AngularFirestore, private alertController: AlertController) {
     this.todosCollection = db.collection<TodoList>('todos');
     this.todosCollection$ = this.todosCollection.snapshotChanges().pipe(
       map(actions => {
@@ -121,25 +122,32 @@ export class TodoServiceProvider {
 
   public newTodoList(name: string) {
     const id: string = uuid();
-    this.todosCollection.doc(id).set({
-      uuid: id,
-      name: name,
-      items: [{
-        uuid: '7dc94eb4-d4e9-441b-b06b-0ca29738c8d2',
-        name: 'Item 1-1',
-        complete: false,
-        desc: 'je suis sans famille et je m appelle remi et je me balade dans la vie'
-      },
-      {
-        uuid: '20c09bdd-1cf8-43b0-9111-977fc4d343bc',
-        name: 'Item 1-2',
-        complete: false
-      },
-      {
-        uuid: 'bef88351-f4f1-4b6a-965d-bb1a4fa3b444',
-        name: 'Item 1-3',
-        complete: true
-      }]
+    const doc = this.todosCollection.doc(name);
+    doc.get().take(1).subscribe( (docSnapshot) => {
+      if (!docSnapshot.exists) {
+        doc.set({
+          uuid: id,
+          name: name,
+          items: [{
+            uuid: '7dc94eb4-d4e9-441b-b06b-0ca29738c8d2',
+            name: 'Item 1-1',
+            complete: false,
+            desc: 'je suis sans famille et je m appelle remi et je me balade dans la vie'
+          },
+          {
+            uuid: '20c09bdd-1cf8-43b0-9111-977fc4d343bc',
+            name: 'Item 1-2',
+            complete: false
+          },
+          {
+            uuid: 'bef88351-f4f1-4b6a-965d-bb1a4fa3b444',
+            name: 'Item 1-3',
+            complete: true
+          }]
+        });
+    } else {
+      this.todoServiceErrorPresentAlert('Impossible de créer deux listes portant le même nom !');
+    }
     });
   }
 
@@ -165,5 +173,15 @@ export class TodoServiceProvider {
   //   this.db.collection('todos', ref => ref.where('uuid', '==', listId))
   //   .snapshotChanges().take(1).subscribe(docs => docs.forEach(v => v.payload.doc.ref.update(newList)));
   // }
+
+  async todoServiceErrorPresentAlert(errorMessage: string) {
+
+    const alert = await this.alertController.create({
+      header : 'Erreur',
+      message : errorMessage,
+      buttons: ['OK']
+    });
+    return await alert.present();
+  }
 
 }
